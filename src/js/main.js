4e7b7838613e6749affe90563a93d242b7465c4e
dimov97 <48334086+dimov97@users.noreply.github.com>
2020-06-04 01:21:56 +0300

    html, css, js

---
+++ src/js/main.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+    var swiper = new Swiper('.s1', {
+        slidesPerView: 3,
+        spaceBetween: 31,
+        loop: true,
+        navigation: {
+            nextEl: '.r1',
+            prevEl: '.l1',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 31
+            },
+            // when window width is >= 640px
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 31
+            }
+        }
+    });
+
+    var swiper2 = new Swiper('.s2', {
+        slidesPerView: 3,
+        spaceBetween: 31,
+        loop: true,
+        navigation: {
+            nextEl: '.r2',
+            prevEl: '.l2',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 31
+            },
+            // when window width is >= 640px
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 31
+            }
+        }
+    });
+    var swiper3 = new Swiper('.s3', {
+        slidesPerView: 3,
+        spaceBetween: 31,
+        loop: true,
+        navigation: {
+            nextEl: '.r3',
+            prevEl: '.l3',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 31
+            },
+            // when window width is >= 640px
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 31
+            }
+        }
+    });
+    AOS.init({
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 700, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+    });
+})
